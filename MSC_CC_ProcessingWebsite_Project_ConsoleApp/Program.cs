b28b8fdb52b1a9ab9aa82b94e86633e9fe60fbb9
Anass El Guermai <aeg@avfuel.com>
2022-10-24 13:52:05 -0400

    Add project files.

---
+++ MSC_CC_ProcessingWebsite_Project_ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+public static class Processor
+{
+    static void Main()
+    {
+        string[] list = Directory.GetFiles("C:\\Repos", "*.*", SearchOption.AllDirectories);
+
+        foreach (string file in list)
+        {
+            Console.WriteLine(file);
+        }
+    }
+}
