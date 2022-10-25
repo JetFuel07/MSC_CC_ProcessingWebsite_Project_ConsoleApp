b3754a71d906ebfa447351791f8f77d54df8f216
Anass El Guermai <aeg@avfuel.com>
2022-10-25 10:36:14 -0400

    1. find files

--- MSC_CC_ProcessingWebsite_Project_ConsoleApp/Program.cs
+++ MSC_CC_ProcessingWebsite_Project_ConsoleApp/Program.cs
@@ -1,14 +1,25 @@
-﻿using System.IO;
+﻿//1. look through the source directory and find all the files matching with file name mask
 
-public static class Processor
+using System.IO;
+
+string[] filePaths = Directory.GetFiles(@"K:\msc transfer\Settlement\2022");
+
+foreach (string filePath in filePaths)
 {
-    static void Main()
-    {
-        string[] list = Directory.GetFiles("C:\\Repos", "*.*", SearchOption.AllDirectories);
-
-        foreach (string file in list)
-        {
-            Console.WriteLine(file);
-        }
-    }
+    Console.WriteLine(filePath);
 }
+
+
+//2. Insert the file name, data load time to the "Processing table"
+
+
+
+//3. Loop through all the files from this run
+
+
+
+//4. Parse the file
+
+
+
+//5. Insert the file to the :"Raw Data table"
